85c9fc817dead0716c3aadcfe0ec7c5d6bcafe33
RazikaBengana <razikabengana@gmail.com>
2024-03-06 15:08:12 +0100

    Try

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -12,9 +12,17 @@
 uint8_t *sha256(const int8_t *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH])
 {
 	if (!digest)
+	{
 		return (NULL);
+	}
 
-	SHA256((const unsigned char *)s, len, digest);
-
-	return (digest);
+	SHA256_CTX context;
+	if (SHA256_Init(&context) && SHA256_Update(&context, s, len) && SHA256_Final(digest, &context))
+	{
+		return (digest);
+	}
+	else
+	{
+		return (NULL);
+	}
 }
