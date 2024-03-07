f48e4a3b9dd3010d52201e092f6235d7e53fbfc6
RazikaBengana <razikabengana@gmail.com>
2024-03-07 19:09:23 +0100

    Test 0. SHA256

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -1,28 +1,29 @@
 #include "hblk_crypto.h"
 
 /**
+ * sha256 - computes the hash of a sequence of bytes
  *
- * @param s
- * @param len
- * @param digest
- * @return
+ * @s: sequence of bytes to be hashed
+ * @len: number of bytes to hash in `s`
+ * @digest: resulting hash stored in this buffer, if not NULL
+ *
+ * Return: a pointer to `digest`, or NULL on failure
  */
-
 
 uint8_t *sha256(const int8_t *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH])
 {
 	if (!digest)
 	{
+		fprintf(stderr, "sha256: NULL digest\n");
 		return (NULL);
 	}
 
-	SHA256_CTX context;
-	if (SHA256_Init(&context) && SHA256_Update(&context, s, len) && SHA256_Final(digest, &context))
+	// Directly compute SHA256 hash into `digest`.
+	if (!SHA256((const unsigned char *)s, len, digest))
 	{
-		return (digest);
-	}
-	else
-	{
+		fprintf(stderr, "sha256: SHA256 function failure\n");
 		return (NULL);
 	}
+
+	return (digest);
 }
