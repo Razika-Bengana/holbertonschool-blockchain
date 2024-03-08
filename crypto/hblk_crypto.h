db1c8f12db8ceddf4c99ba2abf220d149cf3d0eb
RazikaBengana <razikabengana@gmail.com>
2024-03-08 18:47:05 +0100

    Correct Betty style

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -1,6 +1,7 @@
 #ifndef HBLK_CRYPTO_H
 #define HBLK_CRYPTO_H
 
+
 #include <openssl/sha.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -10,26 +11,26 @@
 
 /* EC_KEY public key octet string length (using 256-bit curve) */
 # define EC_PUB_LEN 65
+
 /* Maximum signature octet string length (using 256-bit curve) */
 # define SIG_MAX_LEN    72
 
 # define PRI_FILENAME   "key.pem"
 # define PUB_FILENAME   "key_pub.pem"
 
+
 /**
  * struct sig_s - EC Signature structure
  *
  * @sig: Signature buffer. The whole space may not be used
- * @len: Actual signature size. Can't exceed SIG_MAX_LEN, therefore stored on a byte
+ * @len: Actual signature size. Can't exceed SIG_MAX_LEN,
+ *       therefore stored on a byte
  */
+
 typedef struct sig_s
 {
-    /*
-     * @sig must stay first, so we can directly use the structure as
-     * an array of char
-     */
-    uint8_t     sig[SIG_MAX_LEN];
-    uint8_t     len;
+	uint8_t     sig[SIG_MAX_LEN];
+	uint8_t     len;
 } sig_t;
 
 
