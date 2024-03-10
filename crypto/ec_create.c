a7ae03df7d3c59f99b6088ef1770d5c2f26f0fa5
RazikaBengana <razikabengana@gmail.com>
2024-03-10 12:44:06 +0100

    Betty style

--- crypto/ec_create.c
+++ crypto/ec_create.c
@@ -3,14 +3,17 @@
 /**
  * ec_create - program that creates a new EC key pair
  *
- * We have to use the secp256k1 elliptic curve to create the new pair (See EC_CURVE macro)
+ * We have to use the secp256k1 elliptic curve to create
+ * the new pair (See EC_CURVE macro)
  *
- * Return: a pointer to an EC_KEY structure, containing both the public and private keys, or NULL upon failure
+ * Return: a pointer to an EC_KEY structure, containing both
+ *         the public and private keys, or NULL upon failure
  */
 
 EC_KEY *ec_create(void)
 {
 	EC_KEY *key = EC_KEY_new_by_curve_name(EC_CURVE);
+
 	if (!key)
 	{
 		perror("EC_KEY_new_by_curve_name");
