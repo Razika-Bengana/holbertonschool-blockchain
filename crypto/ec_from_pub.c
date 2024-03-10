90dfc9f6e49d1f8723d3e16b6e76cbde24fa8937
RazikaBengana <razikabengana@gmail.com>
2024-03-10 16:24:50 +0100

    3. EC_KEY from public key

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -5,7 +5,8 @@
  *
  * @pub: the array containing the public key to be converted
  *
- * Return: a pointer to the created EC_KEY structure upon success, or NULL upon failure
+ * Return: a pointer to the created EC_KEY structure upon success,
+ *         or NULL upon failure
  */
 
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
@@ -39,26 +40,15 @@ EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 		return (NULL);
 	}
 
-	if (!EC_POINT_oct2point(group, point, pub, EC_PUB_LEN, NULL))
+	if (!EC_POINT_oct2point(group, point, pub, EC_PUB_LEN,
+				NULL) || !EC_KEY_set_public_key(key, point))
 	{
 		EC_POINT_free(point);
 		EC_GROUP_free(group);
 		EC_KEY_free(key);
-
 		return (NULL);
 	}
-
-	if (!EC_KEY_set_public_key(key, point))
-	{
-		EC_POINT_free(point);
-		EC_GROUP_free(group);
-		EC_KEY_free(key);
-
-		return (NULL);
-	}
-
 	EC_POINT_free(point);
 	EC_GROUP_free(group);
-
 	return (key);
 }
