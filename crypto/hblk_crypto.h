7ed89bf5206bd5d1aa483b4a77f2bbda4f6245c3
RazikaBengana <razikabengana@gmail.com>
2024-03-10 18:44:45 +0100

    Update the header file

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -2,11 +2,17 @@
 #define HBLK_CRYPTO_H
 
 
-#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
 #include <openssl/sha.h>
 #include <openssl/ec.h>
 #include <openssl/obj_mac.h>
+#include <openssl/pem.h>
+#include <errno.h>
+#include <sys/stat.h>
+
 
 
 # define EC_CURVE   NID_secp256k1
@@ -41,14 +47,20 @@ typedef struct sig_s
 uint8_t *sha256(int8_t const *s, size_t len,
 		uint8_t digest[SHA256_DIGEST_LENGTH]);
 
-/* task 1 */
+/* task 0 */
 EC_KEY *ec_create(void);
 
-/* task 2 */
+/* task 1 */
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
 
-/* task 3 */
+/* task 2 */
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
+
+/* task 3 */
+int ec_save(EC_KEY *key, char const *folder);
+
+/* task 4 */
+int ec_save(EC_KEY *key, char const *folder);
 
 
 #endif /* HBLK_CRYPTO_H */
