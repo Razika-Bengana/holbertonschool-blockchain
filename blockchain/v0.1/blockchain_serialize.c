345c609f9c0430abc5930872b1f11d6ffbaec365
RazikaBengana <razikabengana@gmail.com>
2024-03-19 14:27:56 +0100

    Betty style

--- blockchain/v0.1/blockchain_serialize.c
+++ blockchain/v0.1/blockchain_serialize.c
@@ -1,13 +1,22 @@
 #include "blockchain.h"
 
 /**
- * write_block_to_file - program that
+ * write_block_to_file - program that writes a single block to a file
  *
- * @node:
- * @idx:
- * @arg:
+ * This function takes a block from the blockchain, extracts its components,
+ * and writes them to the specified file;
+ * the components include the block's info, data length, data buffer,
+ * and hash;
+ * this function is typically called by llist_for_each() when serializing
+ * the entire blockchain to a file
  *
- * Return:
+ * @node: a pointer to the block to write, casted to a generic pointer
+ * @idx: the index of the block within the blockchain;
+ *       unused in this function, but may be used for logging or other
+ *       purposes in future enhancements
+ * @arg: a pointer to the file (FILE *) where the block data should be written
+ *
+ * Return: 0 on success
  */
 
 int write_block_to_file(llist_node_t node, unsigned int idx, void *arg)
@@ -27,6 +36,24 @@ int write_block_to_file(llist_node_t node, unsigned int idx, void *arg)
 
 	return (0);
 }
+
+
+
+/**
+ * blockchain_serialize - program that serializes the blockchain to a file
+ *
+ * this function opens a file at the given path and writes the blockchain's
+ * metadata (magic number, version, endianness, and number of blocks)
+ * followed by the serialized data of each block;
+ * this serialization includes the block's info, data length, data buffer,
+ * and hash;
+ * the function uses write_block_to_file() to serialize each block
+ *
+ * @blockchain: Aapointer to the blockchain to serialize
+ * @path: the file path where the blockchain should be saved
+ *
+ * Return: 0 on successful serialization, -1 if the file cannot be opened
+ */
 
 int blockchain_serialize(blockchain_t const *blockchain, char const *path)
 {
