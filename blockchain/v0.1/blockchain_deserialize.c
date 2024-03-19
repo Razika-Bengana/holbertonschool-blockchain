beb3ccd6e5b5a1121f630f49e9bbd41aaa42ede7
RazikaBengana <razikabengana@gmail.com>
2024-03-19 22:45:35 +0100

    Betty

--- blockchain/v0.1/blockchain_deserialize.c
+++ blockchain/v0.1/blockchain_deserialize.c
@@ -1,15 +1,16 @@
 #include "blockchain.h"
 
 /**
- * open_and_verify_file - program that opens and verifies a file for blockchain
- * deserialization
+ * open_and_verify_file - program that opens and verifies a file
+ * for blockchain deserialization
  *
- * this function opens a file at the specified path and verifies the header
- * information to ensure it's a valid blockchain file
+ * this function opens a file at the specified path and verifies
+ * the header information to ensure it's a valid blockchain file
  *
  * @path: the path to the file to open and verify
  *
- * Return: a FILE pointer to the opened file upon success, or NULL upon failure
+ * Return: a FILE pointer to the opened file upon success,
+ *         or NULL upon failure
  */
 
 FILE *open_and_verify_file(char const *path)
@@ -45,15 +46,16 @@ FILE *open_and_verify_file(char const *path)
 
 
 /**
- * read_blocks_and_build_blockchain - program that reads blocks from a file and builds
- * a blockchain
+ * read_blocks_and_build_blockchain - program that reads blocks from a file
+ * and builds a blockchain
  *
- * this function reads block data from the provided file and constructs a blockchain
- * structure based on the information read
+ * this function reads block data from the provided file and constructs
+ * a blockchain structure based on the information read
  *
  * @file: the FILE pointer to the file containing block data
  *
- * Return: a pointer to the constructed blockchain upon success, or NULL upon failure
+ * Return: a pointer to the constructed blockchain upon success,
+ *         or NULL upon failure
  */
 
 blockchain_t *read_blocks_and_build_blockchain(FILE *file)
@@ -86,12 +88,14 @@ blockchain_t *read_blocks_and_build_blockchain(FILE *file)
 /**
  * blockchain_deserialize - program that deserializes a Blockchain from a file
  *
- * this function deserializes a Blockchain from a file by opening and verifying the file,
- * reading block data, and building the blockchain structure
+ * this function deserializes a Blockchain from a file by opening and
+ * verifying the file, reading block data, and building the blockchain
+ * structure
  *
  * @path: the path to the file containing the serialized Blockchain
  *
- * Return: a pointer to the deserialized Blockchain upon success, or NULL upon failure
+ * Return: a pointer to the deserialized Blockchain upon success,
+ *         or NULL upon failure
  */
 
 blockchain_t *blockchain_deserialize(char const *path)
@@ -111,5 +115,3 @@ blockchain_t *blockchain_deserialize(char const *path)
 
 	return (blockchain);
 }
-
-
